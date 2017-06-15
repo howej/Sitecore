52f5f0744904b2aee2768eb028a0868742120502
Faihou Ze <faihou@live.com>
2017-06-15 15:10:37 +0800

    Added EditMetaTags button into Sitecore EE.

---
+++ SitecoreX.ExperienceEditor/sitecore modules/SitecoreX/ExperienceEditor/EditMetaTagsButton/EditMetaTagsButton.js
@@ -0,0 +1,17 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js", "/-/speak/v1/ExperienceEditor/ExperienceEditorProxy.js"], function (Sitecore, ExperienceEditor, ExperienceEditorProxy) {
+    Sitecore.Commands.EditMetaTags =
+        {
+            commandContext: null,
+            isEnabled: true,
+            canExecute: function (context) {
+                if (!ExperienceEditor.isInMode("edit") || !context || !context.button || context.currentContext.isFallback) {
+                    return false;
+                }
+
+                return true;
+            },
+            execute: function (context) {
+                ExperienceEditorProxy._pe().postRequest("webedit:fieldeditor(id=" + decodeURI(context.currentContext.itemId) + ",fields=MetaTitle|MetaKeywords|MetaDescription,command={C2332E33-EA84-44DB-A299-FE005BDE22A6})", null, false);
+            }
+        };
+});
